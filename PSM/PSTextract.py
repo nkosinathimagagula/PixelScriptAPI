0bbf20b41ef73253225ca76ee2eb2bcf69c07f11
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-01 00:19:51 +0200

    (update) pass a gray image

--- PSM/PSTextract.py
+++ PSM/PSTextract.py
@@ -2,23 +2,20 @@ import pytesseract as tesseract
 import cv2
 import re
 
-def extract_text(image_file):
+def extract_text(gray_image):
     """
     The return value is a __dict__ with 2 keys 
     - "text"
     - "headings"
 
     Args:
-        image_file (image_file): the image file to extract text from
+        gray image_file (image_file): the image file to extract text from
 
     Returns:
         __dict__: it returns a dictionary with the extracted text and headings
                     extracted from the text.
     """
     heading_regex = r"^[A-Z0-9][^.]*[\n]$"
-    image = cv2.imread(image_file)
-    
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     # Apply the threshold
     _thresh_value, threshed_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -27,4 +24,4 @@ def extract_text(image_file):
     extracted_text = tesseract.image_to_string(threshed_image)
     headings = re.findall(heading_regex, extracted_text, re.MULTILINE)
     
-    return { "text": extract_text, "headings": headings }
+    return { "text": extracted_text, "headings": headings }
