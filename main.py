a6cec8ede14ad69d76c5f82a8dedcc808c2099f5
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-05-31 15:14:32 +0200

    add token generation and authentication

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.param_functions import Body
+from fastapi import FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.param_functions import Depends, Body, Header
 from sqlalchemy.orm import Session
+from datetime import timedelta
 from typing import Annotated
-from database import models, crud
-from database.database import SessionLocal, engine
-from schemas.database_schema import UsersBase, Users
-from schemas.response import Detail
 
 from starlette.responses import JSONResponse
+
+from PSM.PSTextract import extract_text
+from database import models, crud
+from database.database import SessionLocal, engine
+from schemas.database_schema import UsersBase, DataBase, Data
+from schemas.response import Token, Detail
+from security.token import oath2_scheme, ACCESS_TOKEN_EXPIRE_TIME, create_access_token, validate_token
+from security.authenticate import authenticate_user
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -22,6 +29,10 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_token_header(token: Annotated[str, Header()]):
+    return token
         
 ## end of dependencies
 
@@ -29,12 +40,43 @@ def get_db():
 ## test connection !!!
 @app.get("/")
 def home():
-    return JSONResponse(status_code=200, content={"status_code": status.HTTP_200_OK, "message": "success"})
+    return JSONResponse(
+        content={
+            "status_code": status.HTTP_200_OK, 
+            "message": "success"
+        },
+        status_code=200
+    )
 
-####
+
+@app.post("/token", response_model=Token)
+def generate_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+    user = authenticate_user(db=db, email=form_data.username, password=form_data.password)
+    
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username/email or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    
+    access_token_exp = timedelta(minutes=ACCESS_TOKEN_EXPIRE_TIME)
+    
+    access_token = create_access_token(
+        details={"sub": user.email},
+        expires=access_token_exp
+    )
+    
+    return {
+        "access_token": access_token, 
+        "token_type": "bearer"
+    }
+    
+
+
 @app.post("/api/users/", response_model=Detail)
 def create_user(user: Annotated[UsersBase, Body()], db: Session = Depends(get_db)):
-    db_user = crud.get_user(db=db, user=user)
+    db_user = crud.get_user(db=db, email=user.email)
     
     if db_user:
         raise HTTPException(
@@ -48,3 +90,4 @@ def create_user(user: Annotated[UsersBase, Body()], db: Session = Depends(get_db
         content={"detail": user_created},
         status_code=status.HTTP_201_CREATED
     )
+
