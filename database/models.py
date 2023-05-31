e0c53d65aa37191a02b24f8d5142ac0f2f2d6d0a
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-01 00:21:49 +0200

    update models

--- database/models.py
+++ database/models.py
@@ -1,6 +1,9 @@
 from .database import Base
+from datetime import date
+
 from sqlalchemy.schema import Column, ForeignKey
 from sqlalchemy.types import Integer, String, Text, Date
+from sqlalchemy.dialects.mysql import JSON
 
 class Users(Base):
     __tablename__ = "users"
@@ -16,7 +19,8 @@ class Data(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     text = Column(Text, nullable=False)
+    headings = Column(JSON)
     file_type = Column(String(10), nullable=False)
-    date = Column(Date, nullable=False)
+    date = Column(Date, nullable=False, default=date.today())
     
     user_id = Column(Integer, ForeignKey("users.id"))
