3f4ad10769951b0ae4260b7696d082998055efdf
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2023-06-01 10:56:07 +0200

    add crud operations for reading data

--- database/crud.py
+++ database/crud.py
@@ -4,6 +4,7 @@ from datetime import date
 from schemas.database_schema import UsersBase, DataBase
 from security.password_hashing import get_password_hash
 from . import models
+from utils.json import jsonify_data_record
 
 
 def get_user(db: Session, email: str):
@@ -27,13 +28,40 @@ def add_data(db: Session, data: DataBase):
     db.commit()
     db.refresh(data)
     
-    data_dict = {
-        "id": data.id,
-        "text": data.text,
-        "headings": data.headings,
-        "file_type": data.file_type,
-        "date": date.strftime(data.date, "%Y-%m-%d"),
-        "user_id": data.user_id
-    }
+    data_dict = jsonify_data_record(data)
     
     return data_dict
+
+
+def get_data(db: Session, filter: dict):
+    result = []
+    db_response = []
+    
+    if filter['date'] is not None and filter['file_type'] is not None:
+        db_response = db.query(models.Data).filter(models.Data.date <= filter['date'], models.Data.file_type == filter['file_type'], models.Data.user_id == filter['user_id']).all()
+        
+        for record in db_response:
+            result.append(jsonify_data_record(record))
+        
+        return result
+    elif filter['date'] is not None and filter['file_type'] is None:
+        db_response = db.query(models.Data).filter(models.Data.date <= filter['date'], models.Data.user_id == filter['user_id']).all()
+        
+        for record in db_response:
+            result.append(jsonify_data_record(record))
+        
+        return result
+    elif filter['date'] is None and filter['file_type'] is not None:
+        db_response = db.query(models.Data).filter(models.Data.file_type == filter['file_type'], models.Data.user_id == filter['user_id']).all()
+        
+        for record in db_response:
+            result.append(jsonify_data_record(record))
+        
+        return result
+    
+    db_response = db.query(models.Data).filter(models.Data.user_id == filter['user_id']).all()
+    
+    for record in db_response:
+        result.append(jsonify_data_record(record))
+        
+    return result
